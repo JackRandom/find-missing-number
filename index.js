8df71a1cb4c28aa961b7d01dbb6da06e2ded4715
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-08-10 20:24:46 +0100

    Initial implementation

---
+++ index.js
@@ -0,0 +1,13 @@
+// findMissing([1,2,3,4,5,6,8,9,10]) => 7
+
+function findMissing(numberArray) {
+  for (let i = 0; i < numberArray.length; i += 1) {
+    const currentElement = numberArray[i]; // 6
+    const nextElement = numberArray[i + 1]; // 8
+    const correctElement = currentElement + 1; // 7
+
+    if (nextElement !== correctElement) {
+      return correctElement; // aka the one that is missing
+    }
+  }
+}
