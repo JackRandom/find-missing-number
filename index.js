0a07de741aa4835da02bad1fb3152660beb749fd
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-08-11 20:25:28 +0100

    As far as we got in class

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 // findMissing([1,2,3,4,5,6,8,9,10]) => 7
+// findMissing([2,3,4,5,6,7,8,9,10]) => 1
 
-function findMissing(numberArray) {
+const findMissing = (numberArray) => {
+  const firstElement = numberArray[0]; //2
+
+  if (firstElement !== 1) {
+    return 1;
+  }
+
   for (let i = 0; i < numberArray.length; i += 1) {
-    const currentElement = numberArray[i]; // 6
-    const nextElement = numberArray[i + 1]; // 8
-    const correctElement = currentElement + 1; // 7
+    const currentElement = numberArray[i]; // 2
+    const nextElement = numberArray[i + 1]; // 3
+    const correctElement = currentElement + 1; // 3
 
     if (nextElement !== correctElement) {
       return correctElement; // aka the one that is missing
     }
   }
-}
+};
 
 module.exports = findMissing;
--- index.test.js
+++ index.test.js
@@ -1,11 +1,28 @@
 // findMissing([1,2,3,4,5,6,8,9,10]) => 7
+// findMissing([2,3,4,5,6,7,8,9,10]) => 1
+// findMissing([1,3,4,5,6,7,8,9,10]) => 2
+
 const findMissing = require("./index");
 
-describe("finding a number", () => {
-  test("finds missing number 7", () => {
+describe("finding a number", function () {
+  test("finds missing number 7", function () {
     const testArray = [1, 2, 3, 4, 5, 6, 8, 9, 10];
     const result = findMissing(testArray);
 
     expect(result).toBe(7);
+  });
+
+  test("finds missing number 1", function () {
+    const testArray = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const result = findMissing(testArray);
+
+    expect(result).toBe(1);
+  });
+
+  test("finds missing number 1", function () {
+    const testArray = [1, 3, 4, 5, 6, 7, 8, 9, 10];
+    const result = findMissing(testArray);
+
+    expect(result).toBe(2);
   });
 });
